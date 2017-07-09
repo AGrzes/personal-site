52b5a35ca262e8e9759bddb37d190e414c3ad7ab
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-07-09 21:38:17 +0200

    Generator configuration

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-var Metalsmith  = require('metalsmith');
-var layouts     = require('metalsmith-layouts');
-var markdown    = require('metalsmith-markdown');
+var Metalsmith = require('metalsmith');
+var layouts = require('metalsmith-layouts');
+var collections = require('metalsmith-collections');
+var markdown = require('metalsmith-markdown');
+var permalinks = require('metalsmith-permalinks');
 
 Metalsmith(__dirname)
   .source('src')
   .destination('target')
+  .use(collections({ 
+    blog: 'blog/**/*.md'
+  }))
   .use(markdown())
   .use(layouts({
     engine: 'handlebars'
+  }))
+  .use(permalinks({
+    pattern: ':title',
+    linksets: [{
+      match: {
+        collection: 'blog'
+      },
+      pattern: 'blog/:date/:title',
+    }]
   }))
   .build(function (err) {
     if (err) throw err;
