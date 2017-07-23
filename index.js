fc16b353f3718469b60742f4595fc697586aabd7
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-07-23 16:07:16 +0200

    Dedicated layout for blog post and rendering post date

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ var layouts = require('metalsmith-layouts');
 var collections = require('metalsmith-collections');
 var markdown = require('metalsmith-markdown');
 var permalinks = require('metalsmith-permalinks');
+var fileMetadata = require('metalsmith-filemetadata');
+var Handlebars = require("handlebars");
+var MomentHandler = require("handlebars.moment");
+MomentHandler.registerHelpers(Handlebars);
 
 Metalsmith(__dirname)
   .source('src')
   .destination('target')
-  .use(collections({ 
+  .use(fileMetadata([{
+    pattern: "blog/**/*.md",
+    metadata: {
+      "layout": "blog-post.html"
+    }
+  }]))
+  .use(collections({
     blog: 'blog/**/*.md'
   }))
   .use(markdown())
   .use(layouts({
     engine: 'handlebars',
-    default: 'default.html'
+    default: 'default.html',
+    exposeConsolidate: function (requires) {
+      requires.handlebars = Handlebars
+    }
   }))
   .use(permalinks({
     pattern: ':title',
